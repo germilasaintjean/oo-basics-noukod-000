8bd7fc7f1e050b7c8308128df6c7a80c86ce64f4
Saint-Jean Germila <germila@noukod.com>
2018-10-03 17:50:55 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
 #  attr_accessor :color , :size, :materiel , :condition
-    attr_accessor :brand , :color ,:size
-    attr_writer :materiel
+    attr_accessor :brand , :color ,:size, :materiel
+  
   def initialize(brand)
     @brand = brand
   end
