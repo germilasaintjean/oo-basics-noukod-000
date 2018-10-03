e902a0b9e076b94a024b3e4ee2591cd51e46614b
Saint-Jean Germila <germila@noukod.com>
2018-10-03 18:40:15 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe
 #  attr_accessor :color , :size, :materiel , :condition
-    attr_accessor :brand , :color ,:size , :material , :cobble , :condition
+    attr_accessor  :color ,:size , :material , :cobble , :condition
+    attr_reader :brand
   def initialize(brand)
      @brand = brand
   end
