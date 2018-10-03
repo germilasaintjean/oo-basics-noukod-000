9a57d4abb7b91b9638c330d62a609d16ae2c9660
Saint-Jean Germila <germila@noukod.com>
2018-10-03 18:04:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,10 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-
-  def cobble
-    puts "says that the shoe has been repaired"
-  end
+def cobble=(cobble)
+  @cobble = cobble
+end
+def cobble
+puts "says that the shoe has been repaired"
+end
 end
