3bbd4792e8843a54fe0abf40358d9c6907105c07
Saint-Jean Germila <germila@noukod.com>
2018-10-03 18:06:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,10 @@
 class Shoe
 #  attr_accessor :color , :size, :materiel , :condition
-    attr_accessor :brand , :color ,:size , :material , :condition , :cobble
+    attr_accessor :brand , :color ,:size , :material , :cobble , :condition
 
   def initialize(brand)
     @brand = brand
   end
-def cobble=(cobble)
-  @cobble = cobble
-end
 def cobble
 puts "Your shoe is as good as new!"
 end
