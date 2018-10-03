8e909c54843222e46b1a4037c45cc0b129bd883f
Saint-Jean Germila <germila@noukod.com>
2018-10-03 18:00:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,12 @@
 class Shoe
 #  attr_accessor :color , :size, :materiel , :condition
-    attr_accessor :brand , :color ,:size , :material , :condition
+    attr_accessor :brand , :color ,:size , :material , :condition , :cobble
 
   def initialize(brand)
     @brand = brand
   end
 
-
+def cobble
+  puts "says that the shoe has been repaired"
+end
 end
